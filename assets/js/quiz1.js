19e8e55eb5c316ae590ea501dc5af91e06ba8b3c
justkirst <89128835+justkirst@users.noreply.github.com>
2021-11-21 13:28:18 +0000

    Finish CSS styling

---
+++ assets/js/quiz1.js
@@ -0,0 +1,4 @@
+const nextButton = document.getElementById('next-btn')
+const questionContainerElement = document.getElementById('question-container')
+const questionElement = document.getElementById('question')
+const answerButtonsElement = document.getElementById('answer-buttons')
